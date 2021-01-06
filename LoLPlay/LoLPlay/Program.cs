e037eab57a1edbe25b7a9e232bfe7a0fd40bb0ab
Zero Is Black <shlifedev@gmail.com>
2021-01-07 06:05:57 +0900

    .
    .

---
+++ LoLPlay/LoLPlay/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LoLPlay
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+
+            }
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
