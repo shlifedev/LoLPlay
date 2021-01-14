50deb98e634eadd2f86cf4f75aaf3e15e5db007d
Zero Is Black <shlifedev@gmail.com>
2021-01-14 18:11:25 +0900

    command

--- LoLPlay/LoLPlay/Channels/AdminCommandChannel.cs
+++ LoLPlay/LoLPlay/Channels/AdminCommandChannel.cs
@@ -13,20 +13,22 @@ namespace LoLPlay.Channels
             AddCommand("!티어조회");
             AppDomain.CurrentDomain.ProcessExit += OnApplicationQuit;
         }
-        public override async Task OnReceivedMsg(SocketUserMessage message, string command, List<string> args)
+
+        public async Task InquiryLoLTier(ulong discordID)
         {
-            var records = await DB.GetTierVerifyRecordAsync(ulong.Parse(args[0])); 
+            var records = await DB.GetTierVerifyRecordAsync(discordID);
             string tierVerifyLogs = "";
             if (records != null)
             {
                 foreach (var value in records)
-                {
                     tierVerifyLogs += $"`{value.DateTime}  {value.DiscordNick}({value.DiscordID})  {value.LOLNick}({value.SoloTier})`\n";
-                }
             }
-
             await LoLPlayManager.Instance.GetGuild().GetTextChannel(GlobalConfig.AdminCommandChannel).SendMessageAsync(tierVerifyLogs);
-
+        }
+        public override async Task OnReceivedMsg(SocketUserMessage message, string command, List<string> args)
+        {
+            if (command == "!티어조회")
+                await InquiryLoLTier(ulong.Parse(args[0]));
         }
 
         public override void OnApplicationQuit(object sender, EventArgs e)
